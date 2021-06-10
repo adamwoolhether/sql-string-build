85d9cdf145efca6809f9da60bafc0b1b8a02b0c3
adamwoolhether <adamwoolhether@gmail.com>
2021-06-10 08:23:34 +0800

    select * from table where and where

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type variables struct {
+	pwd      string
+	fname    string
+	lname    string
+	location string
+	status   string
+}
+
+func main() {
+	u := variables{
+		pwd:      "",
+		fname:    "adam",
+		lname:    "woolhether",
+		location: "ca",
+		status:   "",
+	}
+	table := "person"
+
+	m := make(map[string]string)
+
+	if u.pwd != "" {
+		m["pwd = "]=u.pwd
+	}
+	if u.fname != "" {
+		m["fname = "] = u.fname
+	}
+	if u.lname != "" {
+		m["lname = "]=u.lname
+	}
+	if u.location != "" {
+		m["location = "]=u.location
+	}
+	if u.status != "" {
+		m["status = "]=u.status
+	}
+
+	out := buildSQL(table, m)
+	fmt.Println(out)
+
+}
+
+func buildSQL(table string, queries map[string]string) string {
+
+	var b strings.Builder
+	b.WriteString("SELECT * FROM " + table)
+
+	if len(queries) > 0 {
+		b.WriteString(" WHERE ")
+		for k, v := range queries {
+			b.WriteString(k+"'"+v+"'"+" AND ")
+		}
+		query := strings.TrimRight(b.String(), " AND ")
+		return query
+	}
+	return b.String()
+}
